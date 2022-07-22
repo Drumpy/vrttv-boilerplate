f7a8b163ba00e7e92af3dcd22529367bfb095dfa
Diego Do Santos <diego.dosantos@interfase.uy>
2022-07-21 21:13:17 -0300

    :bento: fix: update images and reference

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
 import './index.css'
 
+import image from './images/VRTTV.png'
+
 function App() {
   return (
     <main className="grid min-h-screen place-items-center">
       <h1 className="text-center text-3xl font-bold">Hello World! 🥳</h1>
+      <img src={image} />
     </main>
   )
 }
