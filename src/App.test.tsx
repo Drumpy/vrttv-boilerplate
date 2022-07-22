e9e36e6171206d787783c1349a38ebc5e9ffc9ea
Diego Do Santos <diego.dosantos@interfase.uy>
2022-07-21 21:10:36 -0300

    :fire: fix: remove tests files

--- src/App.test.tsx
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react'
-
-import App from './App'
-
-test('that jest is working', () => {
-  render(<App />)
-  expect(screen.getByText('Hello world!')).toBeInTheDocument()
-})
