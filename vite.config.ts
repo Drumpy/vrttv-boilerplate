254f781281f151d7d73d5b10445b6bfeb51236c4
Diego Do Santos <diego.dosantos@interfase.uy>
2022-07-21 21:15:56 -0300

    :heavy_plus_sign: feat: Added viteImagemin and createHtmlPlugin plugins

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,28 @@
 
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
+import { createHtmlPlugin } from 'vite-plugin-html'
+import viteImagemin from 'vite-plugin-imagemin'
 
+import config from './config.js'
 import postcss from './postcss.config.js'
+
+const { imagemin } = config
+
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    viteImagemin(imagemin),
+    createHtmlPlugin({
+      minify: true,
+      // /**
+      //  * After writing entry here, you will not need to add script tags in `index.html`, the original tags need to be deleted
+      //  * @default src/main.ts
+      //  */
+      entry: 'src/main.tsx'
+    })
+  ],
   css: {
     postcss
   }
