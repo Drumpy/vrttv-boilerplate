e4e6e07a98bfbb419102a20c7d2a8705160ca0ee
Diego Do Santos <diego.dosantos@interfase.uy>
2022-07-28 18:17:56 -0300

    :truck: refactor: create folder components and update files

--- src/main.tsx
+++ src/main.tsx
@@ -2,13 +2,12 @@ import './global.css'
 import 'virtual:fonts.css'
 
 import React from 'react'
-import ReactDOM from 'react-dom/client'
+import { createRoot } from 'react-dom/client'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 
-import App from './App'
+import App from './components/App'
 
-// eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-ReactDOM.createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById('root') as Element).render(
   <React.StrictMode>
     <BrowserRouter>
       <Routes>
